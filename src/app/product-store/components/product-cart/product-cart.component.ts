2f253344fdeb6b7e7e61da42df453a762803607d
supatsara <supatsara.rodratsa@gmail.com>
2023-05-25 18:15:24 +0200

    Use pipe to transform price and total price

--- src/app/product-store/components/product-cart/product-cart.component.ts
+++ src/app/product-store/components/product-cart/product-cart.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Cart } from 'src/app/interfaces/product.interface';
 import { ProductStoreService } from '../../services/product-store.service';
+import { DecimalPipe } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-product-cart',
   templateUrl: './product-cart.component.html',
   styleUrls: ['./product-cart.component.css'],
 })
-export class ProductCartComponent {
+export class ProductCartComponent implements OnChanges {
   @Input()
   allCartItems: Cart[] = [];
 
   @Input()
   totalPrice = 0;
 
-  constructor(private productStoreService: ProductStoreService) {}
+  convertTotalPrice: BehaviorSubject<string> = new BehaviorSubject<string>(
+    '0.00',
+  );
+
+  constructor(
+    private productStoreService: ProductStoreService,
+    private decimalPipe: DecimalPipe,
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['totalPrice'].currentValue) {
+      this.convertTotalPrice.next(
+        this.decimalPipe.transform(this.totalPrice, '1.2-2') || '-',
+      );
+    }
+  }
 
   clearCartItems() {
     this.productStoreService.clearCartItems();
--- src/app/product-store/components/product-details/product-details.component.ts
+++ src/app/product-store/components/product-details/product-details.component.ts
@@ -1,11 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnChanges {
   @Input()
   variant: 'cart' | 'catalogue' = 'catalogue';
 
@@ -23,6 +32,18 @@ export class ProductDetailsComponent {
 
   @Output()
   addItem = new EventEmitter<boolean>();
+
+  convertPrice: BehaviorSubject<string> = new BehaviorSubject<string>('0.00');
+
+  constructor(private decimalPipe: DecimalPipe) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['price'].currentValue) {
+      this.convertPrice.next(
+        this.decimalPipe.transform(this.price.toString(), '1.2-2') || '-',
+      );
+    }
+  }
 
   handleAddButtonClick() {
     this.addItem.emit(true);
