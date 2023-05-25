ab0beb25e80b76b7e73d0190c88151909e53d2cf
supatsara <supatsara.rodratsa@gmail.com>
2023-05-25 02:13:15 +0200

    Create Product interface

---
+++ src/app/interfaces/product.interfact.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  rating: Rating;
+}
+
+interface Rating {
+  rate: number;
+  count: number;
+}
