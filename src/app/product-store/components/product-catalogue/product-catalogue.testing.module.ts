f6581457763ff3bc4f370316dedc43753899db73
supatsara <supatsara.rodratsa@gmail.com>
2023-05-30 01:31:48 +0200

    remove unused testing module

--- src/app/product-store/components/product-catalogue/product-catalogue.testing.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ProductCatalogueItemComponent } from '../product-catalogue-item/product-catalogue-item.component';
-
-@NgModule({
-  declarations: [ProductCatalogueItemComponent],
-})
-export class ProductStoreTestingModule {}
