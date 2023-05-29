a47bbf638f2f6e970ebf0b0e2bfeff366d42f46c
supatsara <supatsara.rodratsa@gmail.com>
2023-05-29 09:52:34 +0200

    Remove unnecessary provider pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { CartButtonComponent } from './components/cart-button/cart-button.component';
 @NgModule({
   declarations: [CartButtonComponent],
   imports: [CommonModule],
   exports: [CartButtonComponent],
-  providers: [CurrencyPipe],
 })
 export class SharedModule {}
