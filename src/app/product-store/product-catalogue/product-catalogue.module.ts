d94740cc1b3e7859df5068573bcbf24c2c3eb67e
supatsara <supatsara.rodratsa@gmail.com>
2023-06-02 10:24:59 +0200

    update catalogue module

--- src/app/product-store/product-catalogue/product-catalogue.module.ts
+++ src/app/product-store/product-catalogue/product-catalogue.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductCatalogueItemComponent } from './product-catalogue-item/product-catalogue-item.component';
-import { FilterProductsPipe } from './pipes/filter-products.pipe';
 import { CategoriesPipe } from './pipes/categories.pipe';
 
 @NgModule({
-  declarations: [
-    ProductCatalogueItemComponent,
-    FilterProductsPipe,
-    CategoriesPipe,
-  ],
+  declarations: [ProductCatalogueItemComponent, CategoriesPipe],
   imports: [CommonModule, SharedModule],
-  exports: [ProductCatalogueItemComponent, FilterProductsPipe, CategoriesPipe],
+  exports: [ProductCatalogueItemComponent, CategoriesPipe],
 })
 export class ProductCatalogueModule {}
