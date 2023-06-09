0d4c17aef6e4153ba1c2cca3d139b55b3e3c8eb2
supatsara <supatsara.rodratsa@gmail.com>
2023-06-09 03:34:29 +0200

    create checkout service to transfer data to backend

---
+++ src/app/product/product-store/service/checkout.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  BehaviorSubject,
+  Subscription,
+  catchError,
+  combineLatest,
+  switchMap,
+  take,
+  tap,
+} from 'rxjs';
+import { OrderPurchase } from 'src/app/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product.service';
+import { HOST } from 'src/settings';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CheckoutService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
+  subscription!: Subscription;
+
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService,
+    private router: Router,
+  ) {}
+
+  submitOrderPurchase(data: OrderPurchase) {
+    this.subscription = combineLatest([
+      this.productService.allCartItems,
+      this.productService.totalPrice,
+    ])
+      .pipe(
+        take(1),
+        switchMap(([cartItems, totalPrice]) => {
+          this.loadingSubject.next(true);
+          data.orderItems = cartItems;
+          data.totalPrice = totalPrice;
+          return this.http.post(`${HOST}/orders`, data);
+        }),
+        tap(() => {
+          this.productService.clearCartItems();
+          this.router.navigate(['success']);
+          this.loadingSubject.next(false);
+        }),
+        catchError((error) => {
+          console.error('There is something wrong! ', error);
+          throw error;
+        }),
+      )
+      .subscribe();
+  }
+}
